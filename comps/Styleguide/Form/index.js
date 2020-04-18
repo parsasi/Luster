a6e61f1d6f6a7d6e8e74abdb7d2d1316486500ac
= <=>
2020-04-17 20:55:38 -0700

    styleguide form done

---
+++ comps/Styleguide/Form/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import './form.css';
+
+const Form = ({ color, text, backgroundColor, border }) =>
+  <div id="styleguide_form">
+    <p>First Name</p>
+    <input className="styleguide_inputs1" placeholder="First Name"></input>
+    <p>Email Address</p>
+    <input className="styleguide_inputs1" placeholder="Email Address"></input>
+    <p>Gender</p>
+    <input className="styleguide_inputs2" placeholder="Options"></input>
+    <p>Birthday</p>
+    <input className="styleguide_inputs3" placeholder="Month"></input>
+    <input className="styleguide_inputs3" placeholder="Day"></input>
+    <input className="styleguide_inputs3" placeholder="Year"></input>
+    <p>Preferences</p>
+    <input className="styleguide_inputs2" placeholder="Options"></input>
+  </div>
+
+
+Form.defaultProps = {
+  color: "#FFFFFF",
+  text: "SIGN UP",
+  backgroundColor: "#B2DFDB",
+  border: ""
+}
+
+export default Form;
