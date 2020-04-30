4889185802a19fe2bfe2a22126aa8a76d998eea0
parsasi <parsasi@rocketmail.com>
2020-04-30 01:21:43 -0700

    addUser function is added to the firestore class

--- database/firebase.js
+++ database/firebase.js
@@ -2,28 +2,33 @@ require('dotenv').config()
 
 class Database{
     constructor(){
-        var admin = require("firebase-admin");
+        this.admin = require("firebase-admin");
 
         var serviceAccount = require(process.env.FIREBASE_CONFIG);
         
-        admin.initializeApp({
-          credential: admin.credential.cert(serviceAccount),
+        this.admin.initializeApp({
+          credential: this.admin.credential.cert(serviceAccount),
         });
-        this.db = admin.firestore()
+        this.db = this.admin.firestore()
 
     }
-
     getUsers(){
         return this.db.collection('users').get()
+    }
+    addUser(user){
+      this.db.collection("users").add({
+        ...user
+      })
     }
 }
 
 const db = new Database()
-db.getUsers().then((snapshot) => {
-    snapshot.forEach((doc) => {
-      console.log(doc.id, '=>', doc.data());
-    });
-  })
-  .catch((err) => {
-    console.log('Error getting documents', err);
-  });
+// db.addUser({name : 'Parsa Safavi' , city : 'Vancouver' , dob : db.admin.firestore.Timestamp.fromDate(new Date("May 5, 2001")) , email : 'parsasi@rocketmail.com' , sexualPreference : 'Male' })
+// db.getUsers().then((snapshot) => {
+//     snapshot.forEach((doc) => {
+//       console.log(doc.id, '=>', doc.data());
+//     });
+//   })
+//   .catch((err) => {
+//     console.log('Error getting documents', err);
+//   });
